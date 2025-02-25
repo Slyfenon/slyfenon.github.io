a677dbd6a155eb5784493f395d36cda778848ceb
Nuno Machado <up202206186@up.pt>
2025-02-25 10:25:04 +0000

    Added tangram, misses the other parts of TP2

---
+++ tp2/MyParallelogram.js
@@ -0,0 +1,33 @@
+import { CGFobject } from "../lib/CGF.js";
+
+/**
+ * MyParallelogram
+ * @constructor
+ * @param scene
+ */
+export class MyParallelogram extends CGFobject{
+  constructor(scene){
+    super(scene);
+    this.initBuffers();
+  }
+
+  initBuffers(){
+    this.vertices = [
+      0, 0, 0, 
+      1, 1, 0,
+      3, 1, 0,
+      2, 0, 0
+    ];
+
+    this.indices = [
+      0, 2, 1,
+      0, 3, 2,
+      0 ,1, 2,
+      0, 2, 3
+    ];
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    
+    this.initGLBuffers();
+  }
+}
